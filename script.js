cd3be591885ff61c115de2f1ea50b0b9f5565078
udaymondal <udaysk.mondal@gmail.com>
2021-02-04 14:28:35 +0600

    Get innerText using JavaScript, update text dynamically

--- script.js
+++ script.js
@@ -8,5 +8,19 @@ loginBtn.addEventListener('click', function () {
     console.log("Login Area Vanished, we twicked CSS");
   });
 
+// deposit button event handler
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener('click', function (){
+  const depositAmount = document.getElementById('depositAmount').value;
+  const depositNumber = parseFloat(depositAmount);
+  console.log(depositNumber);
 
+  const currentDeposit = document.getElementById('currentDeposit').innerText;
+  const currentDepositNumber = parseFloat(currentDeposit);
+  const totalDeposit = depositNumber + currentDepositNumber;
+  console.log(totalDeposit);
+
+  document.getElementById('currentDeposit').innerText = totalDeposit;
+  document.getElementById('depositAmount').value = "";
+})
 
